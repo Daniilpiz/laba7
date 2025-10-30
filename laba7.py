74d1c19ff47d6c4f776a52992abb911f2739401e
Даниил <daniildaniil2107@gmail.com>
2025-10-31 00:34:08 +0300

    new file:   laba7.py

---
+++ laba7.py
@@ -0,0 +1,46 @@
+import random as rd
+import numpy as np
+
+from collections import deque
+
+
+def generator_smezh(razm):
+    matr_sm = np.array([abs(rd.randint(-1000, 1000))%2 for _ in range(razm) for _ in range(razm)]).reshape(razm, razm)
+
+    for i in range(razm):
+        matr_sm[i, i] = 0
+        for j in range(i):
+            if i<=j:
+                matr_sm[i, j] = matr_sm[j, i] if True else 0
+
+
+    return matr_sm
+
+
+def search(G: list, visited: list, start: int):
+    visited[start] = True
+    print(f"Посетили узел: {start}")
+
+    for i in range(len(G)):
+        if G[start][i] == 1 and not visited[i]:
+            search(G, visited, i)
+            
+            
+
+    
+def main():
+    razm = int(input("Введите количество вершин:\t"))
+
+    visited = [0 for i in range(razm)]
+    current = int(input("C какой вершины начать?\t"))
+
+
+
+    print(search(generator_smezh(razm), visited, current))
+    
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
