deeda24f7853633216aa502ac2544d4e57eca3bb
Даниил <daniildaniil2107@gmail.com>
2025-11-01 14:53:47 +0300

    Добавил для спискового представления

--- laba7.py
+++ laba7.py
@@ -13,8 +13,8 @@ def generator_smezh(razm):
             if i<=j:
                 matr_sm[i, j] = matr_sm[j, i] if True else 0
 
-
-    return matr_sm
+    print(matr_sm)
+    return matr_sm.tolist()
 
 
 def search(G: list, visited: list, start: int, vyvod):
@@ -28,6 +28,40 @@ def search(G: list, visited: list, start: int, vyvod):
             search(G, visited, i, vyvod)
 
     return vyvod
+
+def matrix_to_adj_list(matrix):
+    """Функциональный стиль преобразования"""
+    return [
+        [j for j in range(len(matrix)) if matrix[i][j] != 0]
+        for i in range(len(matrix))
+    ]
+
+def matrix_to_adj_dict(matrix):
+    n = len(matrix)
+    graph = {}
+    
+    for i in range(n):
+        graph[i] = {}  # инициализируем словарь для вершины i
+        for j in range(n):
+            weight = matrix[i][j]
+            if weight != 0:  # ребро существует
+                graph[i][j] = weight
+    
+    return graph
+
+
+
+def search_2(adj_list: list, visited: list, start: int, vyvod):
+    visited[start] = True
+    vyvod.append(start)
+    print(f"Посетили узел: {start}")
+
+    # Проходим по всем соседям текущей вершины
+    for neighbor in adj_list[start]:
+        if not visited[neighbor]:
+            search_2(adj_list, visited, neighbor, vyvod)
+
+    return vyvod
             
             
 
@@ -37,14 +71,26 @@ def main():
 
 
     G = generator_smezh(razm)
-    visited = [0]*razm
+    visited_1 = [0]*razm
 
-    print(G)
 
     current = int(input("C какой вершины начать?\t"))
 
-    lst = []
-    print(search(G, visited, current, lst))
+    lst_1 = []
+    print(search(G, visited_1, current, lst_1))
+
+
+    G = matrix_to_adj_dict(G)
+    for vertex, neighbors in G.items():
+        print(vertex, neighbors)
+
+
+    current = int(input("C какой вершины начать?\t"))
+    lst_2 = []
+    visited_2 = [0]*razm
+
+    
+    print(search_2(G, visited_2, current, lst_2))
 
     
 
